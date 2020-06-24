b5093918170804b18c816a88bd0ab74f17403b7d
Robson Silva <robsonsilvaof@gmail.com>
2020-06-24 20:46:17 -0300

    Adding Spring Data JPA

---
+++ src/main/java/br/com/robson/forum/repository/TopicoRepository.java
@@ -0,0 +1,13 @@
+package br.com.robson.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.robson.forum.modelo.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
+
+}
